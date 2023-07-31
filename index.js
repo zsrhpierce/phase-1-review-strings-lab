efe9627df24e4ae953eec33f8de26b266b7311c1
Cathy Pierce <zsrhpierce@gmail.com>
2023-07-31 13:22:38 -0600

    strings

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+currentUser.slice(0,2);
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
